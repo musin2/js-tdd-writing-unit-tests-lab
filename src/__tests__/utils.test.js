cec101e48e6c0d734c5032de5a0c036850f58643
musin2 <muhsindaud75@gmail.com>
2024-08-16 23:34:58 +0300

    adds lab code

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,35 @@
-// Your tests here
+import { checkPalindrome } from "../utils";
+
+describe("checkPalindrome", () => {
+  it("returns true when the word is the same forward and backward", () => {
+    const word = "racecar";
+
+    const result = checkPalindrome(word);
+
+    expect(result).toBe(true);
+  });
+  it("returns false when the word is not the same forward and backward", () => {
+    const word = "car";
+
+    const result = checkPalindrome(word);
+
+    expect(result).toBe(false);
+  });
+  it("return true for words that are a combination of uppercase and lowercase letters", () => {
+    const word = "RaCecar";
+
+    const result = checkPalindrome(word);
+
+    expect(result).toBe(true);
+  });
+  it("throws an error if the input has any non-alphabetic characters", () => {
+    const word = "race car";
+
+    expect(() => checkPalindrome(word)).toThrow();
+  });
+  it("throws an error if the input is an empty string", () => {
+    const word = "";
+
+    expect(() => checkPalindrome(word)).toThrow();
+  });
+});
